03d4757a7f507ab156b5a0e217d54f6ab054a4e4
vasant-deb <iamvasantsharma@gmail.com>
2023-04-15 16:19:06 -0600

    update1.0

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://vasantwebdev.info/1stopapi/public/',
-    assetsUrl:'https://vasantwebdev.info/1stopapi/public/uploads/'
+    apiUrl: 'https://api.yusyah.com/public/',
+    assetsUrl:'https://api.yusyah.com/public/uploads/'
   };
   
